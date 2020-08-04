87ddbd67841efe391244c48c09445b1d1b0a52a6
Aniketrana <ranaannu93@gmail.com>
2020-08-04 13:15:22 +0530

    Add project files.

---
+++ BL/Custom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL
+{
+    public class Custom : IEmployee
+    {
+        //public int GetTot()
+        //{
+        //    return 100;
+        //}
+
+        public int Gettotal()
+        {
+            return 100;
+        }
+    }
+}
---
+++ DependencyInjectionInMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DependencyInjectionInMVC.MvcApplication" Language="C#" %>
